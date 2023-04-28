b2f5e3cc6a59a2a8eb278461c59d15592e2fdb45
shivamgupta123456 <sanvi.gupta0096@gmail.com>
2023-04-28 14:39:22 +0530

    First commit

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+//0x6B7f69C9889B22cab6ce01f02E3AC11414e11F1b
